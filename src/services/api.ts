b336b26409c2a8c344f999a4a5cc713dfa1d1e9a
AKSHAY-PV-8 <akshaypv19@gmail.com>
2026-04-10 12:39:48 +0530

    refactor

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,9 +1,10 @@
 import type { ApiResponse } from "../types";
 
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = import.meta.env.VITE_API_URL;
 
-// import.meta.env.VITE_API_URL ||
+
+// "http://localhost:5000";
 
 export const uploadFile = async (file: File): Promise<ApiResponse> => {
   const formData = new FormData();
